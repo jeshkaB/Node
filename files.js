daf03f402858e3450c35def3858a43885a4493a6
jeshkaB <y.bebeshko@ukr.net>
2022-08-12 19:03:14 +0300

    Node HW1

--- files.js
+++ files.js
@@ -1,4 +1,29 @@
 const fs = require('fs').promises;
+//якщо у нас дві окремі папки бойс и герлс:
+// fs.readdir('./boys').then(files => {
+//         for (const file of files) {
+//             fs.readFile(`./boys/${file}`).then(data => {
+//                 if (data.includes('gender:\'female\'')) {
+//                     fs.rename(`./boys/${file}`, `./girls/${file}`).catch(e => {
+//                         console.log(e)
+//                     })
+//                 }
+//             })
+//         }
+//     }
+// )
+// fs.readdir('./girls').then(files => {
+//         for (const file of files) {
+//             fs.readFile(`./girls/${file}`).then(data => {
+//                 if (data.includes('gender:\'male\'')) {
+//                     fs.rename(`./girls/${file}`, `./boys/${file}`).catch(e => {
+//                         console.log(e)
+//                     })
+//                 }
+//             })
+//         }
+//     }
+//)
 
 fs.readdir('./allUsers').then(dirs => {
     for (const dir of dirs) {
